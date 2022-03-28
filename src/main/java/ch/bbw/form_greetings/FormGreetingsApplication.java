9741191b48f1f3990c1693b91ff37cbcb8bd347b
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-03-28 16:03:36 +0200

    Init

---
+++ src/main/java/ch/bbw/form_greetings/FormGreetingsApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.form_greetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormGreetingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormGreetingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/form_greetings/FormGreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.form_greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormGreetingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
